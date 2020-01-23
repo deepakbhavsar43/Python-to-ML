1561650f3c3f31e999d4cc7c501a0548ea210bec
deepakbhavsar43 <deepakbhavsar43@gmail.com>
2020-01-23 21:29:18 +0530

    Add : Added code in file Assessment_5_5

--- Assessment_solution/Assessment_5_3_jan_2020/Assessment_5_4.py
+++ Assessment_solution/Assessment_5_3_jan_2020/Assessment_5_4.py
@@ -1,13 +1,18 @@
 #not yet done
 def fact(n):
-    r = n%10
-    q = n//10
-    temp = temp + r
-    fact(q)
-    # print(r)
-    # print(q)
+    temp = 0
+    if n!=0:
+        r = n%10
+        q = n//10
+        # fact(q)
+        # print('value of r : ', r)
+        # print('value of q : ', q)
+        temp = r + fact(q)
+    # print('value of temp : ',temp)
+    return temp
 
 
+# temp=0
 num = int(input("Enter a number : "))
-fact(num)
-# print(fact(num))
+# fact(num)
+print("The sum of all the digit in the given number is : ",fact(num))
